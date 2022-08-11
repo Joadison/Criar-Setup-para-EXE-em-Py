6be1f44b651367c8d9ff7d389fbd6f640c298f17
JOADISON MS <32674418+Joadison@users.noreply.github.com>
2022-08-11 12:14:31 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+import pyinstaller_versionfile
+
+pyinstaller_versionfile.create_versionfile(
+    output_file="versionfile.txt",
+    version="1.0.0",
+    company_name="JMS",
+    file_description="Suport CATI",
+    internal_name="Suport CATI",
+    legal_copyright="© JMS. Todos os direitos reservados.",
+    original_filename="Suport CATI",
+    product_name="Suport CATI",)
